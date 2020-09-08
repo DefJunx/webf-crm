699902c50796b30297017e5706453adcdf7a736d
Daniele Cencig <hero2frag@gmail.com>
2020-09-08 12:55:11 +0200

    Add Vue app

---
+++ client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <h1>{{ title }}</h1>
+   </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Home extends Vue {
+   title!: string;
+}
+</script>
