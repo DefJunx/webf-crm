503855005de3a6ca34472887644bff95845f4953
Daniele Cencig <hero2frag@gmail.com>
2020-09-08 14:28:40 +0200

    Fix hostname for production

--- client/src/lib/API.ts
+++ client/src/lib/API.ts
@@ -1,5 +1,5 @@
 const API_HOSTNAME =
-   window.location.hostname === "localhost" ? "http://localhost:8080" : "https://webformat-crm.herokuapp.com/";
+   window.location.hostname === "localhost" ? "http://localhost:8080" : "https://webformat-crm.herokuapp.com";
 
 export async function wakeupBackend() {
    return await fetch(`${API_HOSTNAME}/ping`).then(r => r.json());
